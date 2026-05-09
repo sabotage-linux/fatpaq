48000d9efddc242c5b5e7baa49e0ecf1cdfb04de
rofl0r <rofl0r@users.noreply.github.com>
2026-05-09 05:36:38 +0000

    appbundler: support relative symlinks and treat multiple slashes
    on sabotage linux, some broken build systems need to be worked around
    by specifying --prefix=/ - which leads to pathnames starting with two
    slashes, and some of them are even baked into binaries, like
    //lib/libXrender.so. these fell through our case "$fn" in /lib/....
    we now normalize them.
    
    the second case is that sabotage uses relative symlinks.
    in case of /lib/ld-musl-x86_64.so that's a symlink to
    ../opt/musl/lib/ld-musl-x86_64.so which in turn is a symlink to libc.so.
    naively resolving the first symlink with readlink -f will give us
    a target of /opt/musl/lib/libc.so, but not the intermediary symlink pointing
    to it.
    in order to have all of those components, we need to read the link directly,
    then resolve the relative path from the initial filename.

--- appbundler.sh
+++ appbundler.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-# need to hook execve and dump all bins started
-# readelf -l to get interp
 usage() {
 	echo "$0 LOG DIR"
 	echo "LOG is a log of filtrace containing filenames an app accessed"
@@ -9,13 +7,79 @@ usage() {
 	exit 1
 }
 
+resolve_symlink_target() {
+    file=$1
+    target=$2
+
+    # Absolute target: already resolved enough.
+    case $target in
+        /*) printf '%s\n' "$target"; return 0 ;;
+    esac
+
+    # Split file into directory + basename.
+    case $file in
+        */*) dir=${file%/*} ;;
+        *)   dir=. ;;
+    esac
+
+    # Remove leading "../" sequences from target while moving up from file's dir.
+    while :; do
+        case $target in
+            ../*)
+                case $dir in
+                    /*) dir=${dir%/*} ;;
+                    .)  dir=. ;;
+                    *)  dir=${dir%/*} ;;
+                esac
+                target=${target#../}
+                ;;
+            *)
+                break
+                ;;
+        esac
+    done
+
+    # Combine and normalize the remaining path.
+    printf '%s/%s\n' "$dir" "$target" | awk '
+        {
+            n = split($0, a, "/")
+            out_n = 0
+            for (i = 1; i <= n; i++) {
+                p = a[i]
+                if (p == "" || p == ".") continue
+                if (p == "..") {
+                    if (out_n > 0) out_n--
+                    continue
+                }
+                out[++out_n] = p
+            }
+            if ($0 ~ /^\//) printf "/"
+            for (i = 1; i <= out_n; i++) {
+                printf "%s%s", out[i], (i < out_n ? "/" : "")
+            }
+            printf "\n"
+        }
+    '
+}
+
+normalize_leading_slashes() {
+    s=$1
+    while :; do
+        case $s in
+            //* ) s=${s#/} ;;
+            * )   printf '%s\n' "$s"; break ;;
+        esac
+    done
+}
+
 docopy() {
 	fn="$1"
+	echo "copying $fn..."
 	mkdir -p "$DIR""$(dirname "$fn")"
-	cp -a "$fn" "$DIR""$fn"
+	cp -dp "$fn" "$DIR""$fn"
 	if test -L "$fn" ; then
-		targ="$(readlink -f "$fn")"
-		docopy "$targ"
+		targ="$(readlink "$fn")"
+		docopy "$(resolve_symlink_target "$fn" "$targ")"
 	fi
 }
 
@@ -36,6 +100,7 @@ test -e "$fn" || continue
 test -d "$fn" && continue
 test -e "$DIR""$fn" && continue
 copy=false
+fn="$(normalize_leading_slashes "$fn")"
 case "$fn" in
 	/usr/bin/*|/bin/*) copy=true ;;
 	/usr/lib/*|/lib/*) copy=true ;;
